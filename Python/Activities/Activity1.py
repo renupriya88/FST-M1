b2e8956f6e8c6a21ee5801f0027bfe58b84c5e62
renupriya88 <renupriya88@gmail.com>
2024-06-18 14:50:15 +0530

    Activity1

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,6 @@
+username = input("Please enter you name?")
+userage = input("Please enter your age")
+x = 100 - int(userage)
+y = x + 2024
+print(f"{username},You will be 100 years after {x} years.")
+print(f"{username},You will be 100 years in the year {y}")
