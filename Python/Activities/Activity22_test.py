1fe0e7c9a1e63c28862e15dbb14393fb6f234846
renupriya88 <renupriya88@gmail.com>
2024-06-21 11:06:32 +0530

    Activity22

---
+++ Python/Activities/Activity22_test.py
@@ -0,0 +1,21 @@
+import pytest
+
+def test_sum():
+    num = 5
+    assert num + num == 10
+
+def test_diff():
+    num = 4
+    assert num - 2 == 2
+
+@pytest.mark.complex
+def test_mul():
+    num = 4
+    assert num * num == 16
+
+@pytest.mark.complex
+def test_div():
+    num = 25
+    assert num/5 == 5
+
+  
